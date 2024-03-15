868bc225fedd1b855111c6cabbaf62aa31952e1b
Victor Costa <victorcostadecoura@gmail.com>
2024-03-15 10:55:16 -0300

    Controller Advice Created

--- book-registration/src/main/java/com/example/bookregistration/controllers/AplicationControllerAdvice.java
+++ book-registration/src/main/java/com/example/bookregistration/controllers/AplicationControllerAdvice.java
@@ -1,6 +1,8 @@
 package com.example.bookregistration.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.example.bookregistration.exception.RecordNotFoundException;
@@ -8,6 +10,7 @@ import com.example.bookregistration.exception.RecordNotFoundException;
 @RestControllerAdvice
 public class AplicationControllerAdvice {
     @ExceptionHandler(RecordNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public String handleNotFoundException(RecordNotFoundException ex) {
         return ex.getMessage();
     }
