05222d9087248529a8c39b8dd0d33209d2fb8a23
Victor Costa <victorcostadecoura@gmail.com>
2024-03-15 11:20:01 -0300

    AuthorService Updated

--- book-registration/src/main/java/com/example/bookregistration/service/AuthorService.java
+++ book-registration/src/main/java/com/example/bookregistration/service/AuthorService.java
@@ -1,14 +1,15 @@
 package com.example.bookregistration.service;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
-import com.example.bookregistration.domain.author.Author;
 import com.example.bookregistration.domain.author.AuthorRepositoy;
-import com.example.bookregistration.domain.author.RequestAuthor;
+import com.example.bookregistration.domain.author.dto.RequestAuthorDTO;
+import com.example.bookregistration.domain.author.dto.ResponseAuthorDTO;
+import com.example.bookregistration.domain.author.dto.mapper.AuthorMapper;
 import com.example.bookregistration.exception.RecordNotFoundException;
 
 import jakarta.transaction.Transactional;
@@ -20,46 +21,50 @@ import jakarta.validation.constraints.NotNull;
 public class AuthorService {
 
     private final AuthorRepositoy authorRepository;
+    private final AuthorMapper authorMapper;
 
-    public AuthorService(AuthorRepositoy authorRepositoy) {
+    public AuthorService(AuthorRepositoy authorRepositoy, AuthorMapper authorMapper) {
         this.authorRepository = authorRepositoy;
+        this.authorMapper = authorMapper;
     }
 
-    public List<Author> list() {
-        return authorRepository.findAll();
+    public List<ResponseAuthorDTO> list() {
+        return authorRepository.findAll()
+        .stream()
+        .map(authorMapper::toDTO)//.map(author -> authorMapper.toDTO(author))
+        .collect(Collectors.toList());
     }
 
-    public Author findById(@NotNull String id) {
-        return authorRepository.findById(id).orElseThrow(() -> new RecordNotFoundException(id));
+    public ResponseAuthorDTO findById(@NotNull String id) {
+        return authorRepository.findById(id).map(authorMapper::toDTO)//.map(author -> authorMapper.toDTO(author))
+        .orElseThrow(() -> new RecordNotFoundException(id));
     }
 
-    public Author create(@Valid Author author) {
-
-        return authorRepository.save(author);
+    public ResponseAuthorDTO create(@Valid @NotNull RequestAuthorDTO author) {
+        return authorMapper.toDTO(authorRepository.save(authorMapper.toEntity(author)));
     }
 
-    @Transactional
-    public Optional<Author> update(@NotNull String id,@Valid RequestAuthor author) {
+    // @Transactional
+    public ResponseAuthorDTO update(@NotNull String id,@Valid @NotNull RequestAuthorDTO author) {
         return authorRepository.findById(id).map(recordFound -> {
             recordFound.setName(author.name());
             recordFound.setNationality(author.nationality());
-            return recordFound;
-        });
-                
+            return authorMapper.toDTO(recordFound);// return recordFound 
+
+        })// .map(AuthorMapper::toDTO)
+        .orElseThrow(() -> new RecordNotFoundException(id));
+       
     }
 
-    public boolean delete(@NotNull String id) {
-        return authorRepository.findById(id).map(recordFound -> {
-            authorRepository.deleteById(recordFound.getId());
-            return true;
-        }).orElse(false);
+    public void delete(@NotNull String id) {
+        authorRepository.delete(authorRepository.findById(id).orElseThrow(() -> new RecordNotFoundException(id)));
     }
+
     @Transactional
-    public boolean softDelete(@NotNull String id) {
-        return authorRepository.findById(id).map(recordFound -> {
+    public void softDelete(@NotNull String id) {
+       authorRepository.findById(id).map(recordFound -> {
             recordFound.setActive(false);
-            return true;
-        }).orElse(false);
+            return recordFound;
+        }).orElseThrow(() -> new RecordNotFoundException(id));
     }
-
 }
